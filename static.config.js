18953bdd4508327e32dc961c43a7455195f3b1d9
Samuli Suomi <sasuomi@users.noreply.github.com>
2018-03-17 18:16:26 -0700

    Add legacy favicons

--- static.config.js
+++ static.config.js
@@ -33,6 +33,25 @@ export default {
           <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" />
           <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+          <link rel="apple-touch-icon" sizes="57x57" href="/img/favicon/apple-icon-57x57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/img/favicon/apple-icon-60x60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/img/favicon/apple-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="/img/favicon/apple-icon-76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/img/favicon/apple-icon-114x114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/img/favicon/apple-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/img/favicon/apple-icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/img/favicon/apple-icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-icon-180x180.png" />
+          <link rel="icon" type="image/png" sizes="192x192" href="/img/favicon/android-icon-192x192.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/img/favicon/favicon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-TileImage" content="/img/favicon/ms-icon-144x144.png" />
+          <meta name="theme-color" content="#2b404e" />
+          <meta property="og:image" content="http://samulisuomi.fi/img/meta.jpg" />
+
         </Head>
         <Body>
           {children}
