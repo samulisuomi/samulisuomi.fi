3d5a88908503d10e53458a779af3581828a6e36e
Samuli Suomi <sasuomi@users.noreply.github.com>
2018-04-15 21:08:04 -0700

    Update Work.tsx

--- src/pages/Home/Work.tsx
+++ src/pages/Home/Work.tsx
@@ -13,13 +13,8 @@ export default () => (
       Work
     </h4>
     <p>
-      Currently I work as a full-stack web developer at Synack,
+      Currently I do full-stack web development at Synack,
       a Silicon Valley startup providing a crowdsourced security platform.
-    </p>
-    <p>
-      Before this adventure, I worked at the Finnish consulting company Digia, first in enterprise application integrations and then in full-stack web development.
-      I've also researched agile methods,
-      done smaller software projects on the side and completed my M.Sc. in Software Engineering back in 2015.
     </p>
     <p>
       For a full CV, visit my <a href={linkedInUrl}>LinkedIn</a>.
